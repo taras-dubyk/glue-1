ace3113f85cf07689f4f58ec9fd9c67e495db029
Taras Dubyk <taras.dubyk@binary-studio.com>
2019-12-11 17:51:47 +0200

    update script format options config

--- forward_engineering/api.js
+++ forward_engineering/api.js
@@ -20,24 +20,24 @@ module.exports = {
 			const containerData = data.containerData;
 			const entityData = data.entityData;
 
-			if (!data.isUpdateScript) {
-				const script = buildAWSCLIScript(containerData, jsonSchema);
-				callback(null, script);
-				return;
+			if (data.options.targetScriptOptions && data.options.targetScriptOptions.keyword === 'hiveQl') {
+				return callback(null, buildHiveScript(
+					getDatabaseStatement(containerData),
+					getTableStatement(containerData, entityData, jsonSchema, [
+						modelDefinitions,
+						internalDefinitions,
+						externalDefinitions
+					]),
+					getIndexes(containerData, entityData, jsonSchema, [
+						modelDefinitions,
+						internalDefinitions,
+						externalDefinitions
+					])
+				));
 			}
-			callback(null, buildHiveScript(
-				getDatabaseStatement(containerData),
-				getTableStatement(containerData, entityData, jsonSchema, [
-					modelDefinitions,
-					internalDefinitions,
-					externalDefinitions
-				]),
-				getIndexes(containerData, entityData, jsonSchema, [
-					modelDefinitions,
-					internalDefinitions,
-					externalDefinitions
-				])
-			));
+
+			const script = buildAWSCLIScript(containerData, jsonSchema);
+			return callback(null, script);
 		} catch (e) {
 			logger.log('error', { message: e.message, stack: e.stack }, 'AWS Glue -Engineering Error');
 
@@ -55,57 +55,55 @@ module.exports = {
 			const databaseStatement = getDatabaseStatement(containerData);
 			const jsonSchema = parseEntities(data.entities, data.jsonSchema);
 			const internalDefinitions = parseEntities(data.entities, data.internalDefinitions);
-
-			if (!data.isUpdateScript) {
-				const script = buildAWSCLIModelScript(containerData, jsonSchema);
-				callback(null, script);
-				return;
-			}
-
-			const foreignKeyHashTable = foreignKeyHelper.getForeignKeyHashTable(
-				data.relationships,
-				data.entities,
-				data.entityData,
-				jsonSchema,
-				internalDefinitions,
-				[
-					modelDefinitions,
-					externalDefinitions
-				]
-			);
-
-			const entities = data.entities.reduce((result, entityId) => {
-				const args = [
-					containerData,
-					data.entityData[entityId],
-					jsonSchema[entityId], [
-						internalDefinitions[entityId],
+			if (data.options.targetScriptOptions && data.options.targetScriptOptions.keyword === 'hiveQl') {
+				const foreignKeyHashTable = foreignKeyHelper.getForeignKeyHashTable(
+					data.relationships,
+					data.entities,
+					data.entityData,
+					jsonSchema,
+					internalDefinitions,
+					[
 						modelDefinitions,
 						externalDefinitions
 					]
-				];
+				);
+	
+				const entities = data.entities.reduce((result, entityId) => {
+					const args = [
+						containerData,
+						data.entityData[entityId],
+						jsonSchema[entityId], [
+							internalDefinitions[entityId],
+							modelDefinitions,
+							externalDefinitions
+						]
+					];
+	
+					return result.concat([
+						getTableStatement(...args),
+						getIndexes(...args),
+					]);
+				}, []);
+	
+				const foreignKeys = data.entities.reduce((result, entityId) => {
+					const foreignKeyStatement = foreignKeyHelper.getForeignKeyStatementsByHashItem(foreignKeyHashTable[entityId] || {});
+				
+					if (foreignKeyStatement) {
+						return [...result, foreignKeyStatement];
+					}
+	
+					return result;
+				}, []).join('\n');
+	
+				return callback(null, buildHiveScript(
+					databaseStatement,
+					...entities,
+					foreignKeys
+				));
+			}
 
-				return result.concat([
-					getTableStatement(...args),
-					getIndexes(...args),
-				]);
-			}, []);
-
-			const foreignKeys = data.entities.reduce((result, entityId) => {
-				const foreignKeyStatement = foreignKeyHelper.getForeignKeyStatementsByHashItem(foreignKeyHashTable[entityId] || {});
-			
-				if (foreignKeyStatement) {
-					return [...result, foreignKeyStatement];
-				}
-
-				return result;
-			}, []).join('\n');
-
-			callback(null, buildHiveScript(
-				databaseStatement,
-				...entities,
-				foreignKeys
-			));
+			const script = buildAWSCLIModelScript(containerData, jsonSchema);
+			return callback(null, script);
 		} catch (e) {
 			logger.log('error', { message: e.message, stack: e.stack }, 'Cassandra Forward-Engineering Error');
 
